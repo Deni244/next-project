124aa6e19fdb32144eef1971b73eb3fcc6133f51
Ruslan Muliarchuk <rmularcuk9@gmail.com>
2025-02-15 20:48:57 +0200

    change fetch

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,8 +6,14 @@ export const metadata: Metadata = {
     title: "Blog | My Project",
     description: "About company",
   };
+  type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  };
 
-async function getData() {
+async function getData(): Promise<Post[]> {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
         next: {
             revalidate: 60,
@@ -19,12 +25,12 @@ async function getData() {
 }
 
 export default async function Blog() {
-    const posts = await getData();
+    const posts: Post[] = await getData();
     return (
       <>
       <h1>Blog page</h1>
       <ul>
-        {posts.map((post: any) => (
+        {posts.map((post) => (
             <li key={post.id}>
                 <Link href={`/blog/${post.id}`} >{post.title}</Link>
             </li>
